fa7629438b6784ed27c948b83cbb68a460fd74b2
linorise <jongki@hotmail.com>
2023-10-13 11:16:14 +0900

    당첨된 로또 파일 선택 기능 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("jexcelapi:jxl:2.6")
+//    implementation("net.sourceforge.jexcelapi:jxl:2.6.12")
+//    implementation("org.apache.poi:poi:5.2.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("net.sourceforge.jexcelapi:jxl:2.6.12")
 }
--- app/src/main/java/com/rayolla/mylotto/MainActivity.java
+++ app/src/main/java/com/rayolla/mylotto/MainActivity.java
@@ -1,42 +1,139 @@
 package com.rayolla.mylotto;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+//import org.apache.poi.hssf.usermodel.HSSFSheet;
+//import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+//import org.apache.poi.ss.usermodel.Sheet;
+//import org.apache.poi.ss.usermodel.Row;
+//import org.apache.poi.ss.usermodel.Cell;
+//import org.apache.poi.ss.usermodel.Workbook;
+
+//import org.apache.poi.ss.usermodel.Sheet;
+//import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MyLotto_MainActivity";
+    private static final int PICK_FILE_REQUEST_CODE = 100;
+
+    private ArrayList<String> mLottoList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Button button = (Button) findViewById(R.id.button_generate);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
+        Button button = (Button) findViewById(R.id.button_generate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
 
-        readExcel();
+        mLottoList = new ArrayList<String>();
+
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(getApplicationContext(), "Select file", Toast.LENGTH_LONG).show();
+
+                try {
+                    Thread.sleep(2000);
+                }
+                catch (Exception e) {e.printStackTrace();}
+
+                showFileChooser();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_FILE_REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri selectedFileUri = data.getData();
+                Log.d(TAG, "selectedFileUri: " + selectedFileUri);
+
+                if (mLottoList != null) {
+                    mLottoList.clear();
+                }
+
+                try {
+                    InputStream inputStream = getInputStreamFromUri(selectedFileUri);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        Log.d(TAG, "line: " + line);
+                        mLottoList.add(line);
+                    }
+                }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+        }
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//        intent.setType("*/*");
+        intent.setType("text/plain");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select the winning list file"), PICK_FILE_REQUEST_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+        }
+    }
+
+    private InputStream getInputStreamFromUri(Uri uri) {
+        try {
+            return getContentResolver().openInputStream(uri);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public void readExcel() {
         try {
-            InputStream is = getBaseContext().getResources().getAssets().open("test.xls");
-            //엑셀파일
+            InputStream is = getApplicationContext().getResources().getAssets().open("test.xls");
+            Log.d(TAG, "is: " + is);
+
+//            HSSFWorkbook workbook = new HSSFWorkbook(is);
+//            HSSFSheet sheet = workbook.getSheetAt(0);
+//            for (Row row : sheet) {
+//                for (Cell cell : row) {
+//                    String cellValue = cell.toString();
+//                    Log.d(TAG, "cellValue: " + cellValue);
+//                }
+//            }
+//            workbook.close();
+
+//            //엑셀파일
             Workbook wb = Workbook.getWorkbook(is);
             //엑셀 파일이 있다면
             if(wb != null){
@@ -59,7 +156,7 @@ public class MainActivity extends AppCompatActivity {
                             String contents = sheet.getCell(col, row).getContents();
 
                             if(row > 0){
-                                Log.d("Main",  col + "번째: "  + contents);
+                                Log.d(TAG,  col + "번째: "  + contents);
                             }
 
                         }
