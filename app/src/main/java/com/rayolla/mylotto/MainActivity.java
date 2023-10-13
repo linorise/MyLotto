7732853d08c3e3d0ee3d808a76df59be6ab802b8
linorise <jongki@hotmail.com>
2023-10-13 16:49:19 +0900

    임의의 숫자 생성 및 비교 로직 추가

--- app/src/main/java/com/rayolla/mylotto/MainActivity.java
+++ app/src/main/java/com/rayolla/mylotto/MainActivity.java
@@ -9,9 +9,14 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 //import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -30,6 +35,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 import jxl.Sheet;
 import jxl.Workbook;
@@ -40,33 +49,112 @@ public class MainActivity extends AppCompatActivity {
     private static final int PICK_FILE_REQUEST_CODE = 100;
 
     private ArrayList<String> mLottoList = null;
+    private List<Integer> mGenList = null;
+
+    private Button button_generate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button_generate);
-        button.setOnClickListener(new View.OnClickListener() {
+        EditText et_include = (EditText) findViewById(R.id.et_include);
+        InputFilter[] filters = new InputFilter[1];
+        filters[0] = new InputFilter() {
+            public CharSequence filter(CharSequence source, int start, int end,
+                                       Spanned dest, int dstart, int dend) {
+                String filteredText = source.subSequence(start, end).toString();
+
+                String regex = "[0-9,]+";
+                if (filteredText.matches(regex)) {
+                    return null;
+                } else {
+                    return "";
+                }
+            }
+        };
+        et_include.setFilters(filters);
+        et_include.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d(TAG, "onTextChanged: " + s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        EditText et_exclude = (EditText)findViewById(R.id.et_exclude);
+        et_exclude.setFilters(filters);
+        et_exclude.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d(TAG, "onTextChanged: " + s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        button_generate = (Button) findViewById(R.id.button_generate);
+        button_generate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int count = 0;
+//                List<Integer> list = new ArrayList<>();
+
+                do {
+                    int num = getRand();
+                    mGenList.add(num);
+                    count++;
+                } while (count < 6);
+
+                Collections.sort(mGenList);
+                Log.d(TAG, "mGenList:  " + mGenList.toString());
+
+                checkDuplication();
+            }
+        });
+
+        button_generate.setEnabled(false);
+
+        Button button_select = (Button) findViewById(R.id.button_select);
+        button_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
             }
         });
 
         mLottoList = new ArrayList<String>();
+        mGenList = new ArrayList<>();
 
-        runOnUiThread(new Runnable() {
-            public void run() {
-                Toast.makeText(getApplicationContext(), "Select file", Toast.LENGTH_LONG).show();
-
-                try {
-                    Thread.sleep(2000);
-                }
-                catch (Exception e) {e.printStackTrace();}
-
-                showFileChooser();
-            }
-        });
+//        runOnUiThread(new Runnable() {
+//            public void run() {
+//                Toast.makeText(getApplicationContext(), "Select file", Toast.LENGTH_LONG).show();
+//
+//                try {
+//                    Thread.sleep(2000);
+//                }
+//                catch (Exception e) {e.printStackTrace();}
+//
+//                showFileChooser();
+//            }
+//        });
     }
 
     @Override
@@ -87,12 +175,14 @@ public class MainActivity extends AppCompatActivity {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
                     String line;
                     while ((line = reader.readLine()) != null) {
-                        Log.d(TAG, "line: " + line);
+//                        Log.d(TAG, "line: " + line);
                         mLottoList.add(line);
                     }
                 }
                 catch (IOException e) { e.printStackTrace(); }
             }
+
+            button_generate.setEnabled(true);
         }
     }
 
@@ -118,53 +208,44 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void readExcel() {
-        try {
-            InputStream is = getApplicationContext().getResources().getAssets().open("test.xls");
-            Log.d(TAG, "is: " + is);
+    private int getRand() {
+        Random random = new Random();
+        int num;
 
-//            HSSFWorkbook workbook = new HSSFWorkbook(is);
-//            HSSFSheet sheet = workbook.getSheetAt(0);
-//            for (Row row : sheet) {
-//                for (Cell cell : row) {
-//                    String cellValue = cell.toString();
-//                    Log.d(TAG, "cellValue: " + cellValue);
-//                }
-//            }
-//            workbook.close();
+        do {
+            num = random.nextInt(46);
+        } while (num == 0);
 
-//            //엑셀파일
-            Workbook wb = Workbook.getWorkbook(is);
-            //엑셀 파일이 있다면
-            if(wb != null){
+        return num;
+    }
 
-                Sheet sheet = wb.getSheet(0);//시트 블러오기
+    private boolean checkDuplication() {
+        String gen = "";
+        for (int i=0; i<mGenList.size(); i++) {
 
-                if(sheet != null){
-
-                    int colTotal = sheet.getColumns(); //전체 컬럼
-                    int rowIndexStart = 1; //row 인덱스 시작
-                    int rowTotal = sheet.getColumn(colTotal-1).length;
-
-                    StringBuilder sb;
-                    for(int row = rowIndexStart; row < rowTotal; row++){
-
-                        sb = new StringBuilder();
-
-                        //col: 컬럼순서, contents: 데이터값
-                        for(int col = 0; col < colTotal; col++){
-                            String contents = sheet.getCell(col, row).getContents();
-
-                            if(row > 0){
-                                Log.d(TAG,  col + "번째: "  + contents);
-                            }
-
-                        }
-                    }
-                }
+            if (i != mGenList.size() - 1) {
+                gen += mGenList.get(i) + ",";
             }
-        } catch (IOException | BiffException e) {
-            e.printStackTrace();
+            else {
+                gen += mGenList.get(i);
+            }
         }
+
+        // FOR TEST
+//        gen = "10,23,29,33,37,40";
+
+        Log.d(TAG, "gen: " + gen);
+
+        for (int i=0; i<mLottoList.size(); i++) {
+            String str = mLottoList.get(i);
+            Log.d(TAG, String.format("#%d> %s=%s", i, str, gen));
+
+            if (str.equals(gen)) {
+                Log.d(TAG, "Duplication found");
+                return true;
+            }
+        }
+
+        return false;
     }
 }
